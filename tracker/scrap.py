ae4ed1e48b800438cb7d76df1ac572329477d246
tnxcollegeofficial <elijahekongofficial@gmail.com>
2021-03-15 01:42:28 +0100

    initial commit

--- tracker/scrap.py
+++ tracker/scrap.py
@@ -1,20 +1,11 @@
-import builtwith
-import whois
-import re
-import urllib.request
+
+
 import requests
 from bs4 import BeautifulSoup
-
-from lxml import html 
-
-r = requests.get('https://facebook.com/')
+import csv
+import json
+r = requests.get('https://www.tutorialspoint.com/python_web_scraping/python_web_scraping_data_processing.htm')
 soup = BeautifulSoup(r.text, 'lxml')
-url = 'https://authoraditiagarwal.com/leadershipmanagement/'
-path = '//*[@id="panel-836-0-0-1"]/div/div/p[1]'
-response = requests.get(url)
-byte_string = response.content
-source_code = html.fromstring(byte_string)
-tree = source_code.xpath(path)
-print(tree[0].text_content()) 
-
-
+y = json.dumps(soup.title.text)
+with open('JSONFile.txt', 'wt') as outfile:
+   json.dump(y, outfile)
